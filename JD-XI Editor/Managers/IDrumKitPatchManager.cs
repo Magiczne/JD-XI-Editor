c515486e984151c2fe8f3cfbea1cc1dca01af83c
Magiczne <michalkamilkleszczynski@gmail.com>
2018-02-20 10:21:12 +0100

    Added IDrumKitPatchManager

---
+++ JD-XI Editor/Managers/IDrumKitPatchManager.cs
@@ -0,0 +1,17 @@
+﻿using JD_XI_Editor.Models.Patches.DrumKit;
+
+namespace JD_XI_Editor.Managers
+{
+    internal interface IDrumKitPatchManager : IPatchManager
+    {
+        /// <summary>
+        ///     Dump common sysex frame
+        /// </summary>
+        void DumpCommon(Common common, int deviceId);
+
+        /// <summary>
+        ///     Dump partial sysex frame
+        /// </summary>
+        void DumpPartial(Patch patch, string key, int deviceId);
+    }
+}
