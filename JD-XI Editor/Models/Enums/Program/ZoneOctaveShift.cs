26b8e43e6e40911ba2497b84334af15396c2021e
Magiczne <michalkamilkleszczynski@gmail.com>
2018-02-25 10:39:57 +0100

    Program zone model

---
+++ JD-XI Editor/Models/Enums/Program/ZoneOctaveShift.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace JD_XI_Editor.Models.Enums.Program
+{
+    internal enum ZoneOctaveShift : byte
+    {
+        [Description("-3")] MinusThree = 61,
+        [Description("-2")] MinusTwo = 62,
+        [Description("-1")] MinusOne,
+        [Description("0")] Zero,
+        [Description("+1")] PlusOne,
+        [Description("+2")] PlusTwo,
+        [Description("+3")] PlusThree
+    }
+}
---
+++ JD-XI Editor/Models/Patches/Program/Zone.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Windows.Documents;
+using Caliburn.Micro;
+using JD_XI_Editor.Models.Enums.Program;
+using JD_XI_Editor.Utils;
+
+// ReSharper disable InvertIf
+
+namespace JD_XI_Editor.Models.Patches.Program
+{
+    internal class Zone : PropertyChangedBase, IPatch
+    {
+        /// <inheritdoc />
+        public Zone()
+        {
+            Reset();
+        }
+
+        /// <inheritdoc />
+        public void Reset()
+        {
+            ArpeggioSwitch = false;
+            ZoneOctaveShift = ZoneOctaveShift.Zero;
+        }
+
+        /// <inheritdoc />
+        public byte[] GetBytes()
+        {
+            var bytes = new List<byte>();
+
+            bytes.AddRange(ByteUtils.RepeatReserve(3));
+            bytes.Add(ByteUtils.BooleanToByte(ArpeggioSwitch));
+            bytes.AddRange(ByteUtils.RepeatReserve(10));
+
+            bytes.AddRange(ByteUtils.RepeatReserve(11));    //10 * 2 packets + 1
+            bytes.Add((byte) ZoneOctaveShift);
+            bytes.AddRange(ByteUtils.RepeatReserve(9));
+
+            return bytes.ToArray();
+        }
+
+        #region Fields
+
+        /// <summary>
+        ///     Arpeggio Switch
+        /// </summary>
+        private bool _arpeggioSwitch;
+
+        /// <summary>
+        ///     Zone Octave Shift
+        /// </summary>
+        private ZoneOctaveShift _zoneOctaveShift;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Arpeggio Switch
+        /// </summary>
+        public bool ArpeggioSwitch
+        {
+            get => _arpeggioSwitch;
+            set
+            {
+                if (value != _arpeggioSwitch)
+                {
+                    _arpeggioSwitch = value;
+                    NotifyOfPropertyChange(nameof(ArpeggioSwitch));
+                }
+            }
+        }
+
+        /// <summary>
+        ///     Zone Octave Shift
+        /// </summary>
+        public ZoneOctaveShift ZoneOctaveShift
+        {
+            get => _zoneOctaveShift;
+            set
+            {
+                if (value != _zoneOctaveShift)
+                {
+                    _zoneOctaveShift = value;
+                    NotifyOfPropertyChange(nameof(ZoneOctaveShift));
+                }
+            }
+        }
+
+        #endregion
+    }
+}
