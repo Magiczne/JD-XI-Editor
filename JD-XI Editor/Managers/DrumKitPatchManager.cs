c5ba9e1198b8cc94a5db2815582e3ef7f6536954
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2019-01-04 20:53:12 +0100

    Basic drumkit data dump to device

--- JD-XI Editor/Managers/DrumKitPatchManager.cs
+++ JD-XI Editor/Managers/DrumKitPatchManager.cs
@@ -1,13 +1,25 @@
 using System;
 using JD_XI_Editor.Managers.Abstract;
 using JD_XI_Editor.Managers.Events;
+using JD_XI_Editor.Models.Enums.DrumKit;
 using JD_XI_Editor.Models.Patches;
 using JD_XI_Editor.Models.Patches.DrumKit;
+using JD_XI_Editor.Utils;
+using Sanford.Multimedia.Midi;
 
 namespace JD_XI_Editor.Managers
 {
     internal class DrumKitPatchManager : IDrumKitPatchManager
     {
+        #region Fields
+
+        /// <summary>
+        ///     Common address offset
+        /// </summary>
+        private readonly byte[] _commonAddressOffset = {0x19, 0x70, 0x00, 0x00};
+
+        #endregion
+
         #region Events
 
         /// <inheritdoc />
@@ -15,6 +27,18 @@ namespace JD_XI_Editor.Managers
 
         /// <inheritdoc />
         public event EventHandler<TimeoutException> OperationTimedOut;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///     Partial address offset
+        /// </summary>
+        private static byte[] PartialAddressOffset(DrumKey key)
+        {
+            return new byte[] { 0x19, 0x70, (byte) key, 0x00 };
+        }
 
         #endregion
 
@@ -39,13 +63,19 @@ namespace JD_XI_Editor.Managers
         /// <inheritdoc />
         public void DumpCommon(Common common, int deviceId)
         {
-            throw new NotImplementedException();
+            using (var output = new OutputDevice(deviceId))
+            {
+                output.Send(SysExUtils.GetMessage(_commonAddressOffset, common.GetBytes()));
+            }
         }
 
         /// <inheritdoc />
         public void DumpPartial(Partial partial, int deviceId)
         {
-            throw new NotImplementedException();
+            using (var output = new OutputDevice(deviceId))
+            {
+                output.Send(SysExUtils.GetMessage(PartialAddressOffset(partial.Key), partial.GetBytes()));
+            }
         }
 
         #endregion
